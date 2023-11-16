16f361005a8f2a9f072b6364617fb12fa614ff21
Guillermo <guillermo.zarate@sener.es>
2023-11-16 18:13:28 +0100

    new css for loging

--- restaurant_inventory_managment/main/urls.py
+++ restaurant_inventory_managment/main/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('logout', views.logout_request, name='logout'),
     path('inventory/', views.inventory, name="inventory"),
-    path('menu/', views.inventory, name="menu"),
-    path('purchases/', views.inventory, name="purchases"),
+    path('menu/', views.menu, name="menu"),
+    path('purchases/', views.purchases, name="purchases"),
 ]
